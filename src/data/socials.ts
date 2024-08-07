2340154ed44c9be861152afdd71597ec913ce48d
luisjlb <121990027+luisjlb@users.noreply.github.com>
2024-08-06 20:16:48 -0600

    nuevos
    cambios

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,26 +2,32 @@ import IconDribbble from "../components/icons/dribbble.astro";
 import IconGithub from "../components/icons/github.astro";
 import IconLinkedin from "../components/icons/linkedin.astro";
 import IconTwitter from "../components/icons/twitter.astro";
+import Iconcodepen from "../components/icons/codepen.astro";
 
 export const SOCIALS = [
   {
     name: "Github",
-    url: "https://github.com/thevrus",
+    url: "https://github.com/luisjcl",
     icon: IconGithub,
   },
   {
     name: "Linkedin",
-    url: "https://www.linkedin.com/in/vrusin/",
+    url: "https://www.linkedin.com/in/luis-juan-carlos-l%C3%B3pez-beteta-65468a284/",
     icon: IconLinkedin,
   },
   {
     name: "Twitter",
-    url: "https://twitter.com/rusin_vadim",
+    url: "https://x.com/llbeteta",
     icon: IconTwitter,
   },
   {
     name: "Dribbble",
-    url: "https://dribbble.com/iamvr",
+    url: "https://dribbble.com/luisjcl",
     icon: IconDribbble,
+  },
+  {
+    name: "Codepen",
+    url: "https://codepen.io/luis-lopez-beteta",
+    icon: Iconcodepen ,
   },
 ];
